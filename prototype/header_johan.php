2d7b4d6b670038df02b8cd28ea1f4e6f1038f21b
JohanJF <jeanfrancois.johan@stagiairesifa.fr>
2018-11-07 10:48:58 +0100

    johan

---
+++ prototype/header_johan.php
@@ -0,0 +1,27 @@
+		<header>
+			<h1>Le Catalogue des 10 Objets les plus bizarres et inutiles</h1>
+		</header>
+		<nav>
+		<ul>
+			<li><a href="#Accueil">Accueil</a></li>
+			<li>
+				<a href="#Catalogue">Catalogue</a>
+				<ul>
+					<li><a href="#Création">Création</a></li>
+					<li><a href="#Suppression">Suppression</a></li>
+					<li><a href="#Modification">Modification</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#Catégories">Catégories</a>
+				<ul>
+					<li><a href="#Héros">Héros</a></li>
+					<li><a href="#Teams">Teams</a></li>
+					<li><a href="#Les ennemis">Les ennemis</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#Contact">Contact</a>
+			</li>
+		</ul>
+		</nav>
